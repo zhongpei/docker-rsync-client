40a5d44fd5557b427e15f1dd44891786af75665f
jyhl <zhongpei@vip.qq.com>
2020-06-18 09:29:11 +0800

    add close fd

--- run_scripts.py
+++ run_scripts.py
@@ -2,24 +2,26 @@
 
 
 import os
-import sys
-import time
 import os.path
 import subprocess
+import sys
+import time
+
 
 def get_cmd_file(rootdir):
     fnlist = []
-    for parent,dirnames,filenames in os.walk(rootdir):
+    for parent, dirnames, filenames in os.walk(rootdir):
         for filename in filenames:
-            the_file = os.path.join(parent,filename)
+            the_file = os.path.join(parent, filename)
             fnlist.append(the_file)
     return fnlist
 
+
 def run_cmd(rootdir):
     for fn in get_cmd_file(rootdir):
-        command = "/bin/sh %s "%fn
-        print "===>",command
-        subprocess.call(["/bin/sh",fn])
+        command = "/bin/sh {}".format(fn)
+        print("===>", command)
+        subprocess.call(["/bin/sh", fn], close_fds=True)
 
 
 if __name__ == "__main__":
@@ -28,8 +30,8 @@ if __name__ == "__main__":
     if os.getenv('RSYNC_RUN_INTERVAL'):
         try:
             st = int(os.getenv('RSYNC_RUN_INTERVAL'))
-        except:
-            pass
-    print "run interval sec %d"%st
+        except Exception as err:
+            print(err)
+    print("run interval sec {}".format(st))
     run_cmd(rootdir)
     time.sleep(st)
